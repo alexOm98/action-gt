1127aa9593c2a2ff47aa13e22141febe8e3556d6
alexOm98 <95749189+alexOm98@users.noreply.github.com>
2023-03-07 00:00:41 +0300

    Update action.js

--- action.js
+++ action.js
@@ -1,19 +1,21 @@
+const core = require('@actions/core');
 
-const obk ={ name: "SUPERAGENT", login: "SUPERAGENT", password: "SUPERAGENT"}
-async function addNewUser () {
-  let response = await fetch("https://projecct-manager-app.onrender.com/auth/signup", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(obk),
-  })
-let user = await response.json();
-return user;
+const obk = { name: "SUPERAGENT", login: "SUPERAGENT", password: "SUPERAGENT"}
+async function run() {
+  try {
+    let response = await fetch("https://projecct-manager-app.onrender.com/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(obk),
+    })
+  let user = await response.json();
+  return user;
+  }
+  catch (error) {
+    core.setFailed(error.message);
+  }
 }
-try {
-  const newUser = addNewUser();
-  console.log(newUser);
-} catch (error) {
-  console.log('eerr');
-}
+
+run()
